54c0d8a914f24ad6c75f83dc649bb90d8a8f8f92
David Pirih <david.pirih@gmx.de>
2018-11-22 23:38:09 +0100

    TokenGetter, Resolver, UserCards, UserDetails, Tab Style and Img Gallery

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+
 
 export const appRoutes: Routes = [
     /*
@@ -18,7 +22,12 @@ export const appRoutes: Routes = [
         canActivate: [AuthGuard],
         // Append an empty path to child path will finally be the path to be protected.
         children : [
-            { path: 'members', component: MemberListComponent },
+            { path: 'members',
+                component: MemberListComponent,
+                resolve: { users: MemberListResolver }  },
+            { path: 'members/:id',
+                component: MemberDetailComponent,
+                resolve: { user: MemberDetailResolver } },
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent },
         ]
